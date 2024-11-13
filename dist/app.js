866cdb0ed976555cc9e32ef0434f5d299cd5c140
Joao Victor Silva De Oliveira <joao.soliveira@telefonica.com>
2024-11-13 17:28:16 -0300

    Adding build before start command

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,5 @@ const adapter_1 = require("./adapter");
 const request_1 = require("./request");
 console.clear();
 const request = new request_1.Request("http://localhost:4200/test");
-request.setBody({});
 const adapter = new adapter_1.Adapter(request);
-adapter.doPut(100);
+adapter.doGet(100);
